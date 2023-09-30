64126c82a4742aa27464e407982a4cd12805f1cb
luxass <lucasnrgaard@gmail.com>
2023-09-30 12:28:04 +0200

    fix: add closing tags

--- scripts/update-list.ts
+++ scripts/update-list.ts
@@ -39,8 +39,8 @@ async function run() {
     const firstEmoji = emojis[i];
     const secondEmoji = emojis[i + 1];
 
-    newTable += `| ${firstEmoji ? firstEmoji[0] : ""} | ${firstEmoji ? `<img width="20" src="${firstEmoji[1]}">` : ""}`;
-    newTable += `| ${secondEmoji ? secondEmoji[0] : ""} | ${secondEmoji ? `<img width="20" src="${secondEmoji[1]}">` : ""} |\n`;
+    newTable += `| ${firstEmoji ? firstEmoji[0] : ""} | ${firstEmoji ? `<img width="20" src="${firstEmoji[1]}" />` : ""}`;
+    newTable += `| ${secondEmoji ? secondEmoji[0] : ""} | ${secondEmoji ? `<img width="20" src="${secondEmoji[1]}" />` : ""} |\n`;
   }
 
   newTable += "<!-- table end -->";
